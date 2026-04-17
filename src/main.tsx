d553a333e50f1ba9da8e53f1d147c199c0315879
polya2005 <thanpa28@mit.edu>
2026-04-17 16:38:28 -0400

    Replace BrowserRouter with HashRouter in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 import About from "./About.tsx";
@@ -11,16 +11,16 @@ import Misc from "./Misc.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <BrowserRouter>
-          <Routes>
-            <Route element={<App />}>
-              <Route path="/" element={<About />} />
-              <Route path="/chairs" element={<Chairs />} />
-              <Route path="/events" element={<Events />} />
-              <Route path="/photos" element={<Photos />} />
-              <Route path="/misc" element={<Misc />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+        <HashRouter>
+            <Routes>
+                <Route element={<App />}>
+                    <Route path="/" element={<About />} />
+                    <Route path="/chairs" element={<Chairs />} />
+                    <Route path="/events" element={<Events />} />
+                    <Route path="/photos" element={<Photos />} />
+                    <Route path="/misc" element={<Misc />} />
+                </Route>
+            </Routes>
+        </HashRouter>
     </StrictMode>,
 );
